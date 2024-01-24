26f604b8df646448947efaece6036c50d4198226
Jestin <jestinmathew22@gmail.com>
2024-01-24 19:43:14 +0530

    removed unused interceptor

--- src/main/java/com/example/demoallica/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/demoallica/exceptionhandler/GlobalExceptionHandler.java
@@ -4,10 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.HandlerInterceptor;
 
 @ControllerAdvice
-public class GlobalExceptionHandler implements HandlerInterceptor {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleException(ResourceNotFoundException exception) {
